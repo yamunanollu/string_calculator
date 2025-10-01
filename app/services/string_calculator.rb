ff28f67bbfcd2383c5fc3036e8561a80cb759d1d
bharatvb6 <bharatv2544@gmail.com>
2025-10-01 15:18:59 +0530

    Initial commit

---
+++ app/services/string_calculator.rb
@@ -0,0 +1,5 @@
+class StringCalculator
+  def add(numbers)
+    0
+  end
+end
---
+++ spec/services/string_calculator_spec.rb
@@ -0,0 +1,11 @@
+# spec/services/string_calculator_spec.rb
+require 'rails_helper'
+
+RSpec.describe StringCalculator do
+  describe "#add" do
+    it "returns 0 for empty string" do
+      calc = StringCalculator.new
+      expect(calc.add("")).to eq(0)
+    end
+  end
+end
